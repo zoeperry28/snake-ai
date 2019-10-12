0196e9088207e4f9ea07899bf287339d919247d9
zoeperry28 <zoeperry28@gmail.com>
2019-10-12 14:33:52 +0100

    initial base

---
+++ base.cpp
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+char board [32] [32];
+char  start_of_snake_V;
+char start_of_snake_H;
+
+char * snake = new char[30] ;
+int snake_size = 3;
+
+void control_Snake ()
+{
+
+
+  
+}
+
+void create_Snake ()
+{
+  for ( int i = 0 ; i < snake_size; i++ )
+  {
+    snake[i] = '$';
+  }
+}
+
+void add_Food ()
+{
+  int j, k;
+  char * p;
+  srand (time(NULL));
+
+/* generate secret number between 1 and 10: */
+  j = rand() % 30 + 1;
+  k = rand() % 30 + 1;
+  board[j][k] = '%' ;
+
+  start_of_snake_V = rand() % 30 + 1;
+  start_of_snake_H = rand() % 30 + 1;
+  board[start_of_snake_V][start_of_snake_H] = '$' ;
+
+  printf("%x\n%x\n", start_of_snake_V, start_of_snake_H);
+  }
+
+
+char create_Board () {
+
+  int i, j ;
+
+  for (int i = 0; i < 32; i++ )
+  {
+    for (int j = 0; j < 32; j++ )
+    {
+
+      if ( i == 0 ^ j == 0 || i == 31 ^ j == 31)
+      {
+         board[i][j] = '*' ;
+      }
+      else
+      {
+        board[i][j] = '-';
+      }
+
+    }
+  }
+
+  add_Food() ;
+
+      cout<<"\n Two Dimensional Array is : \n";
+      for(i=0; i<32; i++)
+      {
+        for(j=0; j<32; j++)
+         {
+           cout<<" "<<board[i][j]<<" ";
+         }
+          cout<<"\n";
+       }
+
+       return 1;
+
+}
+
+
+
+int main ()
+{
+
+  create_Board();
+
+
+  return 1;
+}
